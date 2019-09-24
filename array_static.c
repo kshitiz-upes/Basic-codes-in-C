378430c70d5bfc803c948db6b058488dc11d6de3
kshitiz gupta <52813499+kshitiz-upes@users.noreply.github.com>
2019-09-24 21:21:07 +0530

    static array in c programming language
    simple programme to print array values in c .
    #code for practise

---
+++ array_static.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+     
+    int arr[5];
+    arr[0]=1;
+    arr[1]=3;
+    arr[2]=5;
+    arr[3]=7;
+    arr[4]=9;
+
+    printf("%d %d %d %d %d\n", arr[0], arr[1], arr[2], arr[3], arr[4] );
+     return 0;
+}
+
