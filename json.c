92032f34d45da3d2929572bc2719faa3ee552253
kshitiz gupta <kshitizgupta82@gmail.com>
2019-10-21 18:48:32 +0530

    Basic programme to show output in json format

---
+++ json.c
@@ -0,0 +1,10 @@
+
+jwOpen( buffer, buflen, JW_OBJECT, JW_PRETTY );  // open root node as object
+jwObj_string( "key", "value" );                  // writes "key":"value"
+jwObj_int( "int", 1 );                           // writes "int":1
+jwObj_array( "anArray");                         // start "anArray": [...] 
+    jwArr_int( 0 );                              // add a few integers to the array
+    jwArr_int( 1 );
+    jwArr_int( 2 );
+jwEnd();                                         // end the array
+err= jwClose();
